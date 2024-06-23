eb9a19e6f7503574869a40d706267625608bd24e
Lucas-Salomao <lucastadeusalomao@gmail.com>
2024-06-23 12:18:05 -0300

    update

--- src/acomodacao/acomodacao.module.ts
+++ src/acomodacao/acomodacao.module.ts
@@ -4,10 +4,11 @@ import { AcomodacaoEntity } from './acomodacao.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AcomodacaoController } from './acomodacao.controller';
 import { NumberIsUniqueValidator } from './validator/numero-is-unique.validator';
+import { AcomodacaoRepository } from './acomodacao.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AcomodacaoEntity])],
   controllers:[AcomodacaoController],
-  providers: [AcomodacaoService, AcomodacaoEntity,NumberIsUniqueValidator]
+  providers: [AcomodacaoService, AcomodacaoEntity,NumberIsUniqueValidator,AcomodacaoRepository]
 })
 export class AcomodacaoModule {}

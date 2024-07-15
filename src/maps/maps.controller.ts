9dc6fc32a75db6cb45e3ee0c0e2401e14e141a73
Lucas-Salomao <lucastadeusalomao@gmail.com>
2024-07-14 21:18:05 -0300

    update

--- src/maps/maps.controller.ts
+++ src/maps/maps.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from '@nestjs/common';
+import { MapsService } from './maps.service';
 
 @Controller('/maps')
 export class MapsController {
-    
+
+    constructor(private readonly mapsService: MapsService) { }
+  @Get('/google-maps-key')
+  async readReserva() {
+    const maps = await this.mapsService.readMaps();
+    return maps;
+  }
 }
--- src/maps/maps.service.ts
+++ src/maps/maps.service.ts
@@ -2,5 +2,9 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class MapsService {
-    
+
+    async readMaps() {
+        const maps={ apiKey: process.env.GOOGLE_MAPS_API_KEY }
+        return maps;
+      }
 }
